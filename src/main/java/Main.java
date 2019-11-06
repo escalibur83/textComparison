235caba3071b91d96eb292dfad97158f9ec942f1
aleksandrharanovich <aleksesc@gmail.com>
2019-11-06 17:43:08 +0300

    Added new test build

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,6 @@ public class Main {
     public static void main (String[] args) throws IOException, GeneralSecurityException, InterruptedException {
         ArrayList<GoogleColumn> allColumn = new ArrayList<GoogleColumn>();
         SheetsQuickstart.readAllDocument("1vpgDfJi0mjvZ8amZ3P9MKjifnbN-11cnDmm6CtRluOE", "Unregulated Help Desktop", 'B', allColumn);
-        OpenToChrome.readToRealGame("http://gc.gaming.skywindgroup.com/lastkingdom/develop/388/index.html", allColumn);
+        OpenToChrome.readToRealGame("http://gc.gaming.skywindgroup.com/lastkingdom/develop/454/index.html", allColumn);
     }
 }
